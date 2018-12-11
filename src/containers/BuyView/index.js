d61f87df16267ded1ea1d6e820436fc5c1e97907
Mark Muskardin <mark.muskardin@gmail.com>
2018-12-11 15:57:35 -0800

    more work on BuyView

--- src/containers/BuyView/index.js
+++ src/containers/BuyView/index.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react'
-import GridList             from '@material-ui/core/GridList'
 import GridListTile         from '@material-ui/core/GridListTile'
 import GridListTileBar      from '@material-ui/core/GridListTileBar'
-import ListSubheader        from '@material-ui/core/ListSubheader'
 import IconButton           from '@material-ui/core/IconButton'
 import InfoIcon             from '@material-ui/icons/Info'
 import accountSvg           from 'assets/svgs/account.svg'
@@ -22,62 +20,62 @@ import { styles }           from './styles.scss'
 const tileData = [
   {
     img: accountSvg,
-    title: 'Image',
+    title: 'Account',
     author: 'author'
   },
   {
     img: imagePlaceholderSvg,
-    title: 'Image',
+    title: 'Image Placeholder',
     author: 'author'
   },
   {
     img: bellCurveSvg,
-    title: 'Image',
+    title: 'Bell Curve',
     author: 'author'
   },
   {
     img: blockchainSvg,
-    title: 'Image',
+    title: 'Blockchain',
     author: 'author'
   },
   {
     img: contractSvg,
-    title: 'Image',
+    title: 'Contract',
     author: 'author'
   },
   {
     img: craneSvg,
-    title: 'Image',
+    title: 'Crane',
     author: 'author'
   },
   {
     img: dependencyGraphSvg,
-    title: 'Image',
+    title: 'Dependency Graph',
     author: 'author'
   },
   {
     img: detectionSvg,
-    title: 'Image',
+    title: 'Detection',
     author: 'author'
   },
   {
     img: lazyLoadingSvg,
-    title: 'Image',
+    title: 'Lazy Loading',
     author: 'author'
   },
   {
     img: loseMoneySvg,
-    title: 'Image',
+    title: 'Lose Money',
     author: 'author'
   },
   {
     img: lockSvg,
-    title: 'Image',
+    title: 'Lock',
     author: 'author'
   },
   {
     img: modularizationSvg,
-    title: 'Image',
+    title: 'Modularization',
     author: 'author'
   }
 ]
@@ -86,26 +84,27 @@ class BuyView extends Component {
   render() {
     return (
       <div className={styles}>
-
-        <GridList cellHeight={180}>
-          <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
-            <ListSubheader component="div">December</ListSubheader>
+        {tileData.map(tile => (
+          <GridListTile
+            key={tile.img}
+            classes={{
+              root: 'gridlisttile-root',
+              tile: 'gridlisttile-tile',
+              imgFullWidth: 'gridlisttile-img'
+            }}
+          >
+            <img src={tile.img} alt={tile.title} />
+            <GridListTileBar
+              title={tile.title}
+              subtitle={<span>by: {tile.author}</span>}
+              actionIcon={
+                <IconButton>
+                  <InfoIcon />
+                </IconButton>
+              }
+            />
           </GridListTile>
-          {tileData.map(tile => (
-            <GridListTile key={tile.img}>
-              <img src={tile.img} alt={tile.title} />
-              <GridListTileBar
-                title={tile.title}
-                subtitle={<span>by: {tile.author}</span>}
-                actionIcon={
-                  <IconButton>
-                    <InfoIcon />
-                  </IconButton>
-                }
-              />
-            </GridListTile>
-          ))}
-        </GridList>
+        ))}
       </div>
     )
   }
